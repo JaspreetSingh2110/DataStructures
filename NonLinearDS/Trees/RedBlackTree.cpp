07de44ff14bc4b1503c2c9805cec735dcb25ce70
Jaspreet Singh <mr.jaspreet2110@gmail.com>
2024-10-16 20:52:19 +0530

    Added implementation for deletion in Red Black tree and fixed issues in insertion

--- NonLinearDS/Trees/RedBlackTree.cpp
+++ NonLinearDS/Trees/RedBlackTree.cpp
@@ -44,11 +44,13 @@ class RedBlackTree {
     LevelOrderTraverse();
   }
 
-  void DeleteNode(int val)
+  bool DeleteNode(int val)
   {
     std::cout << "\nDeleting node with value: " << val << std::endl;
-    Delete(val);
-    LevelOrderTraverse();
+    if (Delete(val) == nil) {
+      return false;
+    }
+    return true;
   }
 
   void LeftRotate(Node* node)
@@ -61,14 +63,15 @@ class RedBlackTree {
 
     rChild->parent = node->parent;
     if (node->parent == nil) {
-      root = node;
+      root = rChild;
     } else if (node == node->parent->left) {
       node->parent->left = rChild;
     } else {
       node->parent->right = rChild;
     }
-    node->parent = rChild;
+
     rChild->left = node;
+    node->parent = rChild;
   }
 
   void RightRotate(Node* node)
@@ -87,8 +90,9 @@ class RedBlackTree {
     } else {
       node->parent->right = lChild;
     }
-    node->parent = lChild;
+
     lChild->right = node;
+    node->parent = lChild;
   }
 
   Node* Insert(int val)
@@ -135,21 +139,17 @@ class RedBlackTree {
 
   void FixInsert(Node* node)
   {
-    //Case 1: If Parent is black, no violation, return.
-    if (node->parent && node->parent->color == BLACK) {
-      return;
-    }
-
     //Case 2: If parent is RED, check for specific case and fix tree.
     while (node->parent->color == RED) {
       //If node's parent is left child of grand parent.
       if (node->parent == node->parent->parent->left) {
         Node* uncle = node->parent->parent->right;
         //Case 2.1: If uncle node is RED in color.
-        if (uncle && uncle->color == RED) {
+        if (uncle->color == RED) {
           uncle->color = BLACK;
           node->parent->color = BLACK;
           node->parent->parent->color = RED;
+          node = node->parent->parent;
         } else {
           //Case 2.2: Uncle node is BLACK in color.
           //Subcase 2.2.1: Node is right child of parent. Rotate left.
@@ -172,10 +172,11 @@ class RedBlackTree {
       else {  // If node's parent is right child of grand parent.
         Node* uncle = node->parent->parent->left;
         //Case 2.1: If uncle node is RED in color.
-        if (uncle && uncle->color == RED) {
+        if (uncle->color == RED) {
           uncle->color = BLACK;
           node->parent->color = BLACK;
           node->parent->parent->color = RED;
+          node = node->parent->parent;
         } else {
           //Case 2.2: Uncle node is BLACK in color.
           //Subcase 2.2.1: Node is left child of parent, Rotate right.
@@ -193,29 +194,153 @@ class RedBlackTree {
 
     //Fix color of root node.
     root->color = BLACK;
+    nil->parent = nullptr;
   }
 
   Node* Delete(int val)
   {
-    if (root == nil) {
-      return nullptr;
+    Node* z = Search(val);
+    if (z == nil) {
+      return nil;
     }
-    return Delete(root, val);
+
+    Node* x;  //This will point to replacement node.
+    Node* y = z; //This points to node to be removed.
+    bool yOrigColor = y->color;
+
+    //BST delete with transplant.
+    //Case 1: Leaf node.
+    if (y->left == nil && y->right == nil) {
+      x = nil;
+      Transplant(y, x);
+    }
+    //Case 2: Node with only left child.
+    else if (y->left == nil) {
+      x = y->right;
+      Transplant(y, x);
+    }
+    //Case 3: Node with only right child.
+    else if (y->right == nil) {
+      x = y->left;
+      Transplant(y,x);
+    }
+    //Case 4: Node with both left and right child.
+    else {
+      //Get the successor. (Min node from right subtree.)
+      // NOTE: We can also use max node from left subtree.
+      y = z->right;
+      while(y->left != nil) {
+        y = y->left;
+      }
+      yOrigColor = y->color;
+
+      //Leftmost node will only have right child (if present)
+      x = y->right;
+      if (y->parent == z) {
+        x->parent = y;
+      } else {
+        Transplant(y, x);
+        y->right = z->right;
+        z->right->parent = y;
+      }
+      Transplant(z,y);
+      y->left = z->left;
+      z->left->parent = y;
+      y->color = z->color;
+    }
+
+    if (yOrigColor == BLACK) {
+      FixDelete(x);
+    }
+    return root;
   }
 
-  Node* Delete(Node* node, int val)
+  void FixDelete(Node* node)
   {
-    return root;
+    Node* x = node;
+    //Repeat this: current node is not root and its color is BLACK.
+    while ((x != root) && (x->color == BLACK)) {
+      //If x is left node of child
+      if (x == x->parent->left) {
+        //Get sibling node. Since x is left child, sibling would be right child of x's parent.
+        Node* s = x->parent->right;
+
+        //Case 1: Sibling color is RED.
+        if (s->color == RED) {
+          s->color = BLACK;
+          x->parent->color = RED;
+          LeftRotate(x->parent);
+          //Reassign sibling pointer 's' to updated sibling of x.
+          s = x->parent->right;
+        }
+        //Case 2: Sibling is black and both children of sibling are black nodes.
+        if ((s->left->color == BLACK) && (s->right->color == BLACK)) {
+          //Update color of sibling as RED.
+          s->color = RED;
+          //Update the x to its parent and next loop will take care its violations.
+          x = x->parent;
+        } else {
+          //Case 3: One of the children of sibling is BLACK.
+          //3.1: If right child of sibling is black.
+          if (s->right->color == BLACK) {
+            s->left->color = BLACK;
+            s->color = RED;
+            RightRotate(s);
+            //Update the sibling pointer.
+            s = s->parent->right;
+          }
+          //3.2: If left child of sibling is black.
+          s->color = x->parent->color;
+          x->parent->color = BLACK;
+          s->right->color = BLACK;
+          LeftRotate(x->parent);
+          x = root;
+        }
+      }
+      else {  //x is right child of its parent.
+        //Since, x is right child of its parent, its sibling will be left child.
+        Node* s = x->parent->left;
+        //Case 1: Sibling is red node.
+        if (s->color == RED) {
+          s->color = BLACK;
+          x->parent->color = RED;
+          RightRotate(x->parent);
+          s = x->parent->left;
+        }
+
+        //Case 2: Sibling is black and color of its both children is BLACK.
+        if ((s->left->color == BLACK) && (s->right->color == BLACK)) {
+          s->color = RED;
+          x = x->parent;
+        } else {
+          //Case 3: One of child of sibling is BLACK.
+          //3.1: Left child is black node.
+          if (s->left->color == BLACK) {
+            s->right->color = BLACK;
+            s->color = RED;
+            LeftRotate(s);
+            s = x->parent->left;
+          }
+
+          s->color = s->parent->color;
+          x->parent->color = BLACK;
+          s->left->color = BLACK;
+          RightRotate(x->parent);
+          x = root;
+        }
+      }
+    } //while loop ends.
+    x->color = BLACK;
   }
 
   RedBlackTree()
   {
     //Create nil node.
-    nil = new Node(0);
+    nil = new Node(-1);
     nil->color = BLACK;
     nil->left = nil;
     nil->right = nil;
-    nil->parent = nil;
+    nil->parent = nullptr;
 
     root = nil;
   }
@@ -236,6 +361,7 @@ class RedBlackTree {
 
       //Print data of current node.
       std::cout << front->key << " ";
+      //std::cout << front->key << " (" << (front->color == BLACK ? "BLACK" : "RED") << ") " << std::endl;
 
       //Add children of current node to queue.
       if (front->left != nil) {
@@ -247,6 +373,58 @@ class RedBlackTree {
     }
   }
 
+  Node* Search(int val)
+  {
+    Node* temp = root;
+    while (temp != nil) {
+      if (val < temp->key) {
+        temp = temp->left;
+      } else if (val > temp->key) {
+        temp = temp->right;
+      } else {
+        return temp;
+      }
+    }
+    return nil;
+  }
+
+  //Primary function to get height of tree.
+  int Height()
+  {
+    return Height(root);
+  }
+
+  //Helper function to get height of tree.
+  int Height(Node *node)
+  {
+    if (node == nil) {
+      return 0;
+    }
+
+    return (1 + std::max(Height(node->left), Height(node->right)));
+  }
+
+  void DeleteTree()
+  {
+    while (root != nil) {
+      DeleteNode(root->key);
+    }
+  }
+
+  void Transplant(Node* u, Node* v)
+  {
+    if (u->parent == nil) {
+      root = v;
+    }
+    else if (u == u->parent->left) {
+      u->parent->left = v;
+    } else {
+      u->parent->right = v;
+    }
+    v->parent = u->parent;
+  }
+
+private:
   Node *root = nullptr;
   Node *nil = nullptr;
 };
@@ -277,22 +455,11 @@ void Test_RedBlackTree()
   std::cout << "\nLevel-Order traversal order : ";
   tree.LevelOrderTraverse();
 
-  /*
-  //Test search of elements in tree.
-  int searchTestElems[] = {99, 34, 5, 4, 89, 45};
-  for (int searchTestElem : searchTestElems) {
-    if (tree.Search(searchTestElem)) {
-      std::cout << "Element [" << searchTestElem << "] is present in tree!!!" << std::endl;
-    }
-    else {
-      std::cout << "Element [" << searchTestElem << "] is not present in tree!!!" << std::endl;
-    }
-  }
-
   //Print height of tree.
   std::cout << "\nHeight of tree is: " << tree.Height() << std::endl;
 
   //Test deletions.
+  /*
   int deleteTestElems[] = {43, 56, 15, 61, 50, 100};
   for (int deleteTestElem : deleteTestElems) {
     std::cout << "\nDeleting element [" << deleteTestElem << "] from tree!!!" << std::endl;
@@ -308,12 +475,12 @@ void Test_RedBlackTree()
     }
 
     std::cout << "Level order traversal of tree after deletion of node: ";
-    tree.Traverse(LEVEL_ORDER);
+    tree.LevelOrderTraverse();
   }
+  */
 
   //Test deletion of tree.
   std::cout << "\nDeleting complete tree i.e. all nodes are getting deleted\n";
   tree.DeleteTree();
   std::cout << "Height of tree after tree deletion: " << tree.Height() << std::endl;
-  */
 }
