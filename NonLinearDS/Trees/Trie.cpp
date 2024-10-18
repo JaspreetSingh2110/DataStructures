93dbf7defc814dc8e58287517ec3d39c732ca76f
Jaspreet Singh <mr.jaspreet2110@gmail.com>
2024-10-18 23:45:09 +0530

    Added implementation for Trie delete

--- NonLinearDS/Trees/Trie.cpp
+++ NonLinearDS/Trees/Trie.cpp
@@ -12,12 +12,23 @@ class TrieNode {
   public:
   TrieNode *data[26];
   bool wordEnd;
+
   explicit TrieNode()
   {
     wordEnd = false;
     for (auto &i : data) {
       i = nullptr;
     }
+  }
+
+  bool IsEmpty()
+  {
+    for (auto &i : data) {
+      if (i != nullptr) {
+        return false;
+      }
+    }
+    return true;
   }
 };
 
@@ -41,6 +52,48 @@ class Trie {
     }
     //When the word completes, set flag to indicate its end.
     cur->wordEnd = true;
+  }
+
+  bool Delete(std::string& word)
+  {
+    TrieNode* res = Delete(root, word, 0);
+    if (res == nullptr) {
+      return false;
+    }
+    return true;
+  }
+
+  TrieNode* Delete(TrieNode* node, std::string& word, int index)
+  {
+    if (node == nullptr) {
+      return nullptr;
+    }
+
+    if (index ==  word.length()) {
+      //If word is found.
+      if (node->wordEnd == true) {
+        //set end node to false.
+        node->wordEnd = false;
+      }
+
+      //Check if any child nodes are present.
+      if (node->IsEmpty()) {
+        delete node;
+        node = nullptr;
+      }
+      return node;
+    }
+
+    //If end node is not reached, move to next character node.
+    char c = word[index];
+    node->data[c - 'a'] = Delete(node->data[c - 'a'], word, index + 1);
+
+    //While traversing back, if node has no child (cause it got deleted), delete it.
+    if ((node->wordEnd == false)  && (node->IsEmpty())) {
+      delete node;
+      node = nullptr;
+    }
+    return node;
   }
 
   bool Search(const std::string& word)
@@ -76,6 +129,27 @@ void Test_Trie()
   std::vector<std::string> sWords = {
     "ant", "an", "jaspreet", "do", "don", "done",  "abcd"
   };
+  for (auto &word : sWords) {
+    if (trie.Search(word)) {
+      std::cout << "Word: '" << word << "' is present in the database!!!" << std::endl;
+    } else {
+      std::cout << "Word: '" << word << "' is not present in the database!!!" << std::endl;
+    }
+  }
+
+  //Test delete
+  std::vector<std::string> dWords = {"ant", "an"};
+  for (auto &word : dWords) {
+    std::cout << "Deleting word : '" << word << "'." << std::endl;
+    if (trie.Delete(word) == false) {
+      std::cout << "Delete failed!!!\n";
+    } else {
+      std::cout << "Delete successful or deletion skipped for word that was not present in DB!!!\n";
+    }
+  }
+
+  //We have attempted to delete words 'ant' and 'an'. Check 'and' is safe in DB.
+  sWords = { "ant", "an", "and"};
   for (auto &word : sWords) {
     if (trie.Search(word)) {
       std::cout << "Word: '" << word << "' is present in the database!!!" << std::endl;
